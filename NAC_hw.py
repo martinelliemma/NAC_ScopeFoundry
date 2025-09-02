29ae62ccbb3d71110a541b6eabff3775e0752696
rime <emma.martinelli@mail.polimi.it>
2025-09-02 16:19:30 +0200

    NAC upgrade

--- NAC_hw.py
+++ NAC_hw.py
@@ -8,7 +8,7 @@ from mimetypes import inited
 
 from ScopeFoundry import HardwareComponent
 #from NAC_device import NeoAndorDevice                          # for NAC_app
-from Andor_ScopeFoundry.NAC_device import NeoAndorDevice        # for SPIM_app
+from NAC_ScopeFoundry_update.NAC_device import NeoAndorDevice        # for SPIM_app
 
 class NeoAndorHW(HardwareComponent):
     name='NeoAndorHW'
